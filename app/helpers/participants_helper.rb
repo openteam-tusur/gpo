c1d306015b5c9e64a4b1a7ec97f01dfb6093f232
Evgeniy Lapin <lev@openteam.ru>
2010-06-30 09:54:40 +0700

    исправлен вывод информации о том, что студент не числится в гпо

--- app/helpers/participants_helper.rb
+++ app/helpers/participants_helper.rb
@@ -2,11 +2,11 @@ module ParticipantsHelper
   def participant_cancel_state_button(button_name, participant)
     participant_change_state_form(cancel_chair_project_participant_url(participant.project.chair, participant.project, participant), button_name)
   end
-  
+
   def participant_approve_state_button(button_name, participant)
     participant_change_state_form(approve_chair_project_participant_url(participant.project.chair, participant.project, participant), button_name)
   end
-  
+
   def participant_change_state_form(url, button_name)
     out = ""
     out << form_tag(url, :method => :put)
@@ -16,6 +16,6 @@ module ParticipantsHelper
   end
 
   def problem_text(participant)
-    participant.problems.collect { |p| icon(:warning) + "<span>#{p}</span>" }.join
+    participant.problems.collect { |p| icon(:warning) + content_tag(:span, p) }.join
   end
 end
