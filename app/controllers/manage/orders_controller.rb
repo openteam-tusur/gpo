c16c80b42750ced9bdeeed852eed3ae1f06689ec
Manin Iliy <bkmzvf@gmail.com>
2012-11-02 09:00:34 +0700

    just a little refactoring

--- app/controllers/manage/orders_controller.rb
+++ app/controllers/manage/orders_controller.rb
@@ -36,45 +36,40 @@ class Manage::OrdersController < Manage::ApplicationController
     }
   end
 
+  #TODO: Не генерить файлик, если он есть на файловой системе
   def send_odt
     filename = "order_#{@order.id}.odt"
+    generated_odt = generate_odt
+    send_file generated_odt.path, :type => 'application/zip', :disposition => 'attachment', :filename => filename
+    generated_odt.unlink
+  end
 
-    zip = Zip::ZipFile.open("#{Rails.root}/lib/templates/reports/#{@order.class.name.underscore}.odt")
-    erb = ERB.new File.read(Rails.root.join("lib", "templates", "reports", @order.class.name.underscore, "content.xml"))
+  #TODO: Не конвертировать, если есть на файловой системе
+  def send_converted_odt(format)
+    filename = "order_#{@order.id}.doc"
+    generated_odt = generate_odt
+    send_report generated_odt.path, :doc, filename
+    generated_odt.unlink
+  end
 
-    t = Tempfile.new("my-temp-filename-#{Time.now}")
+  private
+    def generate_odt
+      zip = Zip::ZipFile.open("#{Rails.root}/lib/templates/reports/#{@order.class.name.underscore}.odt")
+      erb = ERB.new File.read(Rails.root.join("lib", "templates", "reports", @order.class.name.underscore, "content.xml"))
 
-    Zip::ZipOutputStream.open(t.path) do |z|
-      zip.each do |entry|
-        z.put_next_entry entry.name
-        if entry.name == 'content.xml'
-          z.write erb.result(binding)
-        else
-          z.print zip.read(entry.name)
+      generated_odt = Tempfile.new("my-temp-filename-#{Time.now}")
+
+      Zip::ZipOutputStream.open(generated_odt.path) do |z|
+        zip.each do |entry|
+          z.put_next_entry entry.name
+          if entry.name == 'content.xml'
+            z.write erb.result(binding)
+          else
+            z.print zip.read(entry.name)
+          end
         end
       end
+
+      generated_odt
     end
-
-    #TODO: Не генерить файлик, если он есть на файловой системе
-    send_file t.path, :type => 'application/zip', :disposition => 'attachment', :filename => filename
-
-    t.unlink
-    zip.close
-  end
-
-  def send_converted_odt(format)
-    filename = "order_#{@order.id}.#{format.to_s}"
-    converted_file = Tempfile.new('converted_file')
-    if @order.file?
-      system("bash", "#{RAILS_ROOT}/script/converter/converter.sh", @order.file.to_file.path, converted_file.path, format.to_s)
-      send_file converted_file.path, :type => Mime::Type.lookup_by_extension(format.to_s), :filename => filename
-      converted_file.close
-    else
-      @order.generate_odt_file do |file|
-        system("bash", "#{RAILS_ROOT}/script/converter/converter.sh", file.path, converted_file.path, format.to_s)
-        send_file converted_file.path, :type => Mime::Type.lookup_by_extension(format.to_s), :filename => filename
-        converted_file.close
-      end
-    end
-  end
 end
