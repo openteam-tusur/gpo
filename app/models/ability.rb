76d38ab189f06f70be1994df06513ff2682b92f1
Dmitry Lihachev <lda@openteam.ru>
2012-11-09 09:29:12 +0700

    миграция ролей

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,37 +4,13 @@ class Ability
   def initialize(user)
     return unless user
 
-    can :manage, :all
-
-    ## common
-    can :manage, Context do | context |
-      user.manager_of? context
-    end
-
-    can :manage, Permission do | permission |
-      permission.context && user.manager_of?(permission.context)
-    end
-
-    can [:new, :create], Permission do | permission |
-      !permission.context && user.manager?
-    end
-
-    can [:search, :index], User do
-      user.manager?
-    end
+    can :manage, :all if user.manager?
 
     can :manage, :application do
       user.have_permissions?
     end
 
-    can :manage, :permissions do
-      user.manager?
-    end
-
-    can :manage, :audits do
-      user.manager_of? Context.first
-    end
-
     ## app specific
+
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -5,8 +5,8 @@ require 'spec_helper'
 describe Ability do
   context 'менеджер проекта' do
     let(:project) { Fabricate(:project) }
-    subject { ability_for(manager_of(project)) }
+    subject { ability_for(project_manager_of(project)) }
 
-    it { should be_able_to(:manage, another_manager_of(project).permissions.first) }
+    it { should_not be_able_to(:manage, another_manager_of(project).permissions.first) }
   end
 end
