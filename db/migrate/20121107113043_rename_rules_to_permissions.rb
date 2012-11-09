be67062cdbaf31d014eae2fc81ea23d24c36cbce
Dmitry Lihachev <lda@openteam.ru>
2012-11-09 11:58:09 +0700

    fixup migrations

--- db/migrate/20121107113043_rename_rules_to_permissions.rb
+++ db/migrate/20121107113043_rename_rules_to_permissions.rb
@@ -7,7 +7,5 @@ class RenameRulesToPermissions < ActiveRecord::Migration
     Permission.where(:created_at => nil).update_all(:created_at => DateTime.parse('2009-01-28 10:35:55'))
     Permission.where(:updated_at => nil).update_all('updated_at = created_at')
     add_index :permissions, [:user_id, :role, :context_id, :context_type], :name => 'by_user_and_role_and_context'
-    Permission.where(:role => :admin).update_all(:role => :manager)
-    User.find_by_id(1).try :update_attribute, :uid, 1
   end
 end
