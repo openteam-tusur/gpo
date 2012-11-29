e8d29f6f0ec4f169f1024eed65f11676c415bab3
Dmitry Lihachev <lda@openteam.ru>
2012-11-29 08:50:07 +0700

    allow nil values for {participant,manager}#{first,last,middle}_name

--- app/grape/api/entities/participant_entity.rb
+++ app/grape/api/entities/participant_entity.rb
@@ -1,5 +1,5 @@
 class API::Entities::ParticipantEntity < Grape::Entity
-  expose :first_name,   :format_with => ->(s) { s.squish }
-  expose :last_name,    :format_with => ->(s) { s.squish }
-  expose :middle_name,  :format_with => ->(s) { s.squish }
+  expose :first_name,   :format_with => ->(s) { s.try(:squish) }
+  expose :last_name,    :format_with => ->(s) { s.try(:squish) }
+  expose :middle_name,  :format_with => ->(s) { s.try(:squish) }
 end
