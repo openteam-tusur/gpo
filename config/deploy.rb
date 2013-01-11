72d38af3cfa5db675860b2abae945461dd42ecb8
Dmitry Lihachev <lda@openteam.ru>
2013-01-11 09:36:12 +0700

    + files, generated by openteam-commons

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,15 +13,14 @@ namespace :deploy do
     run "ln -s #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
   end
 
-  desc "HASK copy right unicorn.rb file"
-  task :copy_config do
-    run "ln -s #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/unicorn.rb"
-    run "ln -s #{deploy_to}/shared/config/directories.rb #{deploy_to}/current/config/directories.rb"
-  end
-
   desc "Reload Unicorn"
   task :reload_servers do
     sudo "/etc/init.d/#{unicorn_instance_name} restart"
+  end
+
+  desc "Update crontab tasks"
+  task :crontab do
+    run "cd #{deploy_to}/current && exec bundle exec whenever --update-crontab --load-file #{deploy_to}/current/config/schedule.rb"
   end
 
   desc "Airbrake notify"
@@ -33,9 +32,9 @@ end
 # deploy
 after "deploy:finalize_update", "deploy:config_app"
 after "deploy", "deploy:migrate"
-after "deploy", "deploy:copy_config"
 after "deploy", "deploy:reload_servers"
 after "deploy:restart", "deploy:cleanup"
+after "deploy", "deploy:crontab"
 after "deploy", "deploy:airbrake"
 
 # deploy:rollback
