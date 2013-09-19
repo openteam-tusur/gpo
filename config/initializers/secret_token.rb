ce6abc75a1380c00751a2472ce66e7b15a45b472
Evgeny Lapin <lev@openteam.ru>
2013-09-19 12:33:49 +0700

    update rails

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Gpo::Application.config.secret_token = '4b8e3afc6208819dfaf2e60421d9a7a05cefbd534589ad136b1763b47c12a9341d5bba3750e18cdca1da7e33d0647069bf2e067525737ffd381f8613ae56c1f7'
+# Gpo::Application.config.secret_token = '4b8e3afc6208819dfaf2e60421d9a7a05cefbd534589ad136b1763b47c12a9341d5bba3750e18cdca1da7e33d0647069bf2e067525737ffd381f8613ae56c1f7'
