c6d19b7e5453febc8b3ae45a10f6f2f27a44c942
Manin Iliy <bkmzvf@gmail.com>
2014-10-13 12:43:36 +0700

    fix authorization on manage namespace

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -1,8 +1,14 @@
 class Manage::ApplicationController < ApplicationController
+  before_action :check_permissions
+
   rescue_from CanCan::AccessDenied do |exception|
     flash['error'] = t('cancan.access_denied')
     redirect_to root_url
   end
 
   layout 'application'
+
+  def check_permissions
+    authorize! :manage, :application
+  end
 end
