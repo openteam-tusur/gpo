dc2bf71f9508d20991d9de34114c42b59281ae62
maximkuzmin <photographer.max@gmail.com>
2016-05-05 16:30:20 +0600

    Certificate template and generation changed

--- lib/certificate_pdf.rb
+++ lib/certificate_pdf.rb
@@ -6,7 +6,6 @@ class CertificatePdf
 
   def make_pdf
     @pdf = Prawn::Document.new(page_size: 'A4', margin: 0)
-    pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
     @certificates.each{ |certificate| generate_page certificate }
     pdf.render
   end
@@ -15,20 +14,37 @@ class CertificatePdf
 
   def generate_page(object)
     pdf.image "#{Rails.root}/lib/pdf_templates/#{resolve_image(object)}", at: [0, 841], width: 595, height: 841
-    pdf.move_down 270
-    pdf.text object.participant.last_name, align: :center, size: 24
-    pdf.text [object.participant.first_name,object.participant.middle_name].join(" "), align: :center, size: 24
-    pdf.bounding_box([90, 420], width: 410) do
-      pdf.text object.participant.project.title, align: :center, size: 12
-    end
+    pdf.bounding_box([90, 580], width: 410) do
+      pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
+      pdf.text object.participant.last_name, align: :center, size: 24
+      pdf.text [object.participant.first_name,object.participant.middle_name].join(" "), align: :center, size: 24
 
-    pdf.bounding_box([90, 286], width: 410) do
+      pdf.move_down 20
+      pdf.font "#{Rails.root}/app/assets/fonts/officinaserifboldosc.ttf"
+      pdf.text "в процессе обучения принимал/а участие в разработке проекта", align: :center, size: 16, color: '034f9b'
+
+      pdf.move_down 10
+      pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
+      pdf.text "#{object.participant.project.title} (#{object.participant.project.cipher})", align: :center, size: 12
+
+      pdf.move_down 25
+      pdf.font "#{Rails.root}/app/assets/fonts/officinaserifboldosc.ttf"
+      pdf.text "проект выполнялся", align: :center, size: 16, color: '034f9b'
+
+      pdf.move_down 10
+      pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
       pdf.text object.project_reason, align: :center, size: 12
-    end
 
-    pdf.bounding_box([90, 195], width: 410) do
-      pdf.text object.project_result, align: :center, size: 12
-    end unless object.project_result.blank?
+      unless object.project_result.blank?
+        pdf.move_down 25
+        pdf.font "#{Rails.root}/app/assets/fonts/officinaserifboldosc.ttf"
+        pdf.text "результат выполнения проекта", align: :center, size: 16, color: '034f9b'
+
+        pdf.move_down 10
+        pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
+        pdf.text object.project_result, align: :center, size: 12
+      end
+    end
 
     pdf.start_new_page if certificates.last != object
   end
