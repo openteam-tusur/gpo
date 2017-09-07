502cc809ba4bb8668594b815b2ef95d43f10e8b5
Evgeniy Lapin <lev@openteam.ru>
2017-09-07 11:50:59 +0700

    rescue from ActionController::InvalidAuthenticityToken

--- app/controllers/manage/application_controller.rb
+++ app/controllers/manage/application_controller.rb
@@ -1,6 +1,12 @@
 class Manage::ApplicationController < ApplicationController
   before_action :check_permissions
 
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    flash[:error] = 'Что-то пошло не так с вашей сессией, попробуйте войти еще раз'
+
+    redirect_to root_path
+  end
+
   rescue_from CanCan::AccessDenied do |exception|
     flash['error'] = t('cancan.access_denied')
     redirect_to root_url
