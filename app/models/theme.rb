9364ac4bb777b37fd21813ccc69c6bfa0de70ec3
Evgeniy Lapin <lev@openteam.ru>
2017-09-12 13:02:35 +0700

    removed deprication warning

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,13 +1,13 @@
 class Theme < ActiveRecord::Base
   attr_accessible :name
 
-  has_many :projects, :order => 'cipher'
-  has_many :participants, :through => :projects
+  has_many :projects, -> { order(:cipher) }
+  has_many :participants, through: :projects
 
   validates_presence_of :name
   validates_uniqueness_of :name
 
-  default_scope order(:name)
+  default_scope { order(:name) }
 end
 
 # == Schema Information
