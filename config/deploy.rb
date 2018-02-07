68af232a32c34857f124d2467449985d1ceb51e3
Evgeniy Lapin <lev@openteam.ru>
2018-02-07 11:03:31 +0700

    added linked dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,15 @@
 require 'openteam/capistrano/deploy'
 
+append :linked_dirs, 'system'
+
+desc 'Download files'
+task :download_files do
+  on roles(:all) do |host|
+    download! "#{shared_path}/system", '.', via: :scp, recursive: :true
+  end
+end
+
+
 set :slackistrano,
   channel: (Settings['slack.channel'] rescue ''),
   webhook: (Settings['slack.webhook'] rescue '')
