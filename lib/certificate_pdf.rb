0e0991d4cca715e4a1b2ad6c859277d4b3f839de
zzorin <ilya-zorin@yandex.ru>
2018-11-30 17:00:05 +0700

    changed certificate template

--- lib/certificate_pdf.rb
+++ lib/certificate_pdf.rb
@@ -14,14 +14,20 @@ class CertificatePdf
 
   def generate_page(object)
     pdf.image "#{Rails.root}/lib/pdf_templates/#{resolve_image(object)}", at: [0, 841], width: 595, height: 841
-    pdf.bounding_box([90, 580], width: 410) do
+    pdf.bounding_box([50, 580], width: 495) do
+
+      pdf.move_down 20
+      pdf.font "#{Rails.root}/app/assets/fonts/officinaserifboldosc.ttf"
+      pdf.text "свидетельствует о том, что", align: :center, size: 16, color: '034f9b'
+
+      pdf.move_down 10
       pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
       pdf.text object.participant.last_name, align: :center, size: 24
       pdf.text [object.participant.first_name,object.participant.middle_name].join(" "), align: :center, size: 24
 
       pdf.move_down 20
       pdf.font "#{Rails.root}/app/assets/fonts/officinaserifboldosc.ttf"
-      pdf.text "в процессе обучения принимал/а участие в разработке проекта", align: :center, size: 16, color: '034f9b'
+      pdf.text "в процессе обучения принимал участие в разработке проекта", align: :center, size: 16, color: '034f9b'
 
       pdf.move_down 10
       pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
@@ -47,7 +53,7 @@ class CertificatePdf
     end
     pdf.bounding_box([90, 72], width: 410) do
       pdf.font "#{Rails.root}/app/assets/fonts/PTSans.ttf"
-      pdf.text "Томск - #{Date.today.year} г.", align: :center, size: 16
+      pdf.text "Томск - #{Date.today.year}", align: :center, size: 16
     end
 
     pdf.start_new_page if certificates.last != object
