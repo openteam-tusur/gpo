6081a5107f2e1bdab6b62f8156b90a1815b247a9
zzorin <ilya-zorin@yandex.ru>
2019-06-21 10:10:22 +0700

    added locales for chair_attestation models

--- app/controllers/manage/reports_controller.rb
+++ app/controllers/manage/reports_controller.rb
@@ -48,7 +48,9 @@ class Manage::ReportsController < Manage::ApplicationController
 
   def edit_chair_attestation
     find_chair
+
     @stages = @chair.projects.active.map{|p| p.current_attestation_stage}
+    @stage_title = @stages.first.title + ' по кафедре ' + @chair.abbr
   end
 
   def update_schedule_group
