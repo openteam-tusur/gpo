702f11804fc04955b669a48b0ec7d439fbc462ef
zzorin <ilya-zorin@yandex.ru>
2019-07-05 15:07:16 +0700

    working attachment for stage_achievements

--- app/models/stage_achievement.rb
+++ app/models/stage_achievement.rb
@@ -1,10 +1,14 @@
 class StageAchievement < ActiveRecord::Base
   attr_accessible :kind,
                   :title,
-                  :stage_id
+                  :stage_id,
+                  :scan
 
   belongs_to :stage
 
-  validates_presence_of :stage_id
-  has_attached_file :attachment
+  validates_presence_of :stage_id, :title, :kind
+  has_attached_file :scan, {
+    path: 'system/:class/:attachment/:date/:id/:filename',
+    url: '/system/:class/:attachment/:date/:id/:filename'
+  }
 end
