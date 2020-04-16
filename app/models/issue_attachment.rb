4fa03640691997195c47a0c68b74167dc3d8f0ff
zzorn <ilya-zorin@yandex.ru>
2020-04-16 13:52:07 +0700

    changed issue_attachment paths

--- app/models/issue_attachment.rb
+++ app/models/issue_attachment.rb
@@ -5,8 +5,8 @@ class IssueAttachment < ActiveRecord::Base
   belongs_to :issue
   validates_presence_of :title, :document_file_name
   has_attached_file :document, {
-    path: 'system/:class/:attachment/:date/:id/:filename',
-    url: '/system/:class/:attachment/:date/:id/:filename'
+    path: 'system/:class/:attachment/:id/:filename',
+    url: '/system/:class/:attachment/:id/:filename'
   }
 
   def project
