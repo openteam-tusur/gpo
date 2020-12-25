11357f82c665518c59f4c10b699968b9eaa29a5a
Gelani Geliskhanov <1a6_geliskhanov@mail.ru>
2020-12-25 13:52:58 +0700

    edit action was added for project_manager

--- app/models/attestation_mark.rb
+++ app/models/attestation_mark.rb
@@ -7,7 +7,7 @@ class AttestationMark < ActiveRecord::Base
   belongs_to :stage
 
   validates_presence_of :stage_id, :participant_id, :mark
-  validates_inclusion_of :mark, :in => 1..30
+  validates_inclusion_of :mark, :in => 0..30
   validates_uniqueness_of :participant_id, :scope => [:stage_id], :message => 'Баллы для участника уже выставлены'
 end
 
