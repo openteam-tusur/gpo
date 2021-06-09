9bfd204fd268dcca5d3cd818a284ea4e5d7e72d9
Evgeniy Lapin <lev@openteam.ru>
2021-06-09 17:19:14 +0700

    reject distance faculty

--- app/controllers/chairs_controller.rb
+++ app/controllers/chairs_controller.rb
@@ -5,7 +5,9 @@ class ChairsController < ApplicationController
   layout 'public'
 
   def index
-    @chairs = Chair.ordered_by_faculty.group_by(&:faculty)
+    @chairs = Chair.ordered_by_faculty.group_by(&:faculty).reject{ |item|
+      item =~ /Факультет дистанционного обучения/i
+    }
   end
 
 end
