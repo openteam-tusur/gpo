a28ca839896f35033adf5fc1339a084469c98e5e
Gelani Geliskhanov <1a6_geliskhanov@mail.ru>
2021-06-22 14:35:16 +0700

    error with scan url was fixed

--- app/models/stage_achievement.rb
+++ app/models/stage_achievement.rb
@@ -13,6 +13,18 @@ class StageAchievement < ActiveRecord::Base
     path: 'system/:class/:attachment/:date/:id/:filename',
     url: '/system/:class/:attachment/:date/:id/:filename'
   }
+
+  def scan_url
+    year = updated_at.to_date.strftime("%Y")
+    month = updated_at.to_date.strftime("%m")
+    day = updated_at.to_date.strftime("%d")
+    str = scan.url
+    splitted_str = str.split('/')
+    splitted_str[4] = year
+    splitted_str[5] = month
+    splitted_str[6] = day
+    splitted_str.join('/')
+  end
 end
 
 # == Schema Information
